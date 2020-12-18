fe137a109d6c6622f2900219b062018ccba95fb1
Angel's Work Station (Coding) <angelsworkstation@Angels-iMac.local>
2020-12-18 12:39:57 -0800

    [App]Create nowCurrentUser, handleLogout, and useEffect for authentication

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+// IMPORTS
 import React, { useEffect, useState } from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import jwt_decode from 'jwt-decode';
+import setAuthToken from './utils/setAuthToken';
+
+// CSS
 import './App.css';
+
+// COMPONENTS
 import Welcome from './components/Welcome';
 import About from './components/About';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
+
+
+
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
   const user = localStorage.getItem('jwtToken');
@@ -15,9 +24,44 @@ const PrivateRoute = ({ component: Component, ...rest }) => {
 }
 
 function App() {
+
+  // SET STATE VALUES
+  const [currentUser, setCurrentUser] = useState('');
+  const [isAuthenticated, setIsAuthenticated] = useState(true);
+
+  useEffect(() => {
+    let token;
+
+    // if there is no token in localStorage, then the user is not authenticated
+    if(!localStorage.getItem('jwtToken')) {
+      setIsAuthenticated(false);
+    } else {
+      token = jwt_decode(localStorage.getItem('jwtToken'));
+      setAuthToken(localStorage.jwtToken);
+      setCurrentUser(token);
+    }
+  }, []);
+
+
+  const nowCurrentUser = (userData) => {
+    console.log('nowCurrentUser is here...');
+    setCurrentUser(userData);
+    setIsAuthenticated(true);
+  } 
+
+  const handleLogout = () => {
+    if (localStorage.getItem('jwtToken')) {
+      localStorage.removeItem('jwtToken')
+      setCurrentUser(null)
+      setIsAuthenticated(false);
+    }
+  }
+
+
+
   return (
     <div className="App">
-      <Navbar />
+      <Navbar handleLogout={handleLogout} isAuth={isAuthenticated} />
       <Welcome />
       <About />
       <Footer />
