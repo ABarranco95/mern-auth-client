ec026c4177acbd6fcc11c3e427ade385a01703dd
Angel's Work Station (Coding) <angelsworkstation@Angels-iMac.local>
2020-12-18 09:17:19 -0800

    [components] Add about and footer components

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, sit facilis hic blanditiis voluptatem optio magnam sint rerum perferendis nobis quasi nemo? Dolores a quo aut praesentium veniam repellat quis.</p>
+        </div>
+    )
+}
+
+export default About;
