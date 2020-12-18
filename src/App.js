fdfa09e24b46a5bc5df1a30edbf6843c9e27756b
Angel's Work Station (Coding) <angelsworkstation@Angels-iMac.local>
2020-12-18 12:15:36 -0800

    [App] Create PrivateRoute

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import React, { useEffect, useState } from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import jwt_decode from 'jwt-decode';
 import './App.css';
-
 import Welcome from './components/Welcome';
 import About from './components/About';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  const user = localStorage.getItem('jwtToken');
+  return <Route {...rest } render={(props) => {
+    return user ? <Component {...rest} {...props} /> : <Redirect to='/login' />
+  }}/>
+}
 
 function App() {
   return (
